f3913e1dfe92ac47f890db676203662be8cd617b
Chris Anderson <jchris@couchbase.com>
2014-03-10 05:32:11 -0700

    index page to create a room

--- page/js/main.js
+++ page/js/main.js
@@ -8,5 +8,12 @@ $(function () {
       App({id : match[1]}),
       document.getElementById('container')
     );
+  } else {
+    $("form input[type=text]").val(Math.random().toString(26).substr(2))
+    $("form").on("submit", function(e){
+      e.preventDefault();
+      var room = $("form input[type=text]").val();
+      document.location = "/talk/" + room;
+    })
   }
 })
