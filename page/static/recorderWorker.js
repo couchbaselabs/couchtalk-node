e2b5c76764165243ead74c41371f3c7e56d5399a
Chris Anderson <jchris@couchbase.com>
2014-03-14 23:26:07 -0700

    16bit mono

--- page/static/recorderWorker.js
+++ page/static/recorderWorker.js
@@ -150,7 +150,7 @@ function encodeWAV(samples){
 }
 
 function encodeMonoWAV(samples){
-  var buffer = new ArrayBuffer(44 + samples.length);
+  var buffer = new ArrayBuffer(44 + samples.length * 2);
   var view = new DataView(buffer);
 
   /* RIFF identifier */
@@ -174,50 +174,13 @@ function encodeMonoWAV(samples){
   /* block align (channel count * bytes per sample) */
   view.setUint16(32, 2, true);
   /* bits per sample */
-  view.setUint16(34, 8, true);
+  view.setUint16(34, 16, true);
   /* data chunk identifier */
   writeString(view, 36, 'data');
   /* data chunk length */
-  view.setUint32(40, samples.length, true);
+  view.setUint32(40, samples.length * 2, true);
 
-  floatTo8BitPCM(view, 44, samples);
+  floatTo16BitPCM(view, 44, samples);
 
   return view;
 }
-
-
-// function encodeMonoWAV(samples){
-//   var buffer = new ArrayBuffer(44 + samples.length);
-//   var view = new DataView(buffer);
-
-//   /* RIFF identifier */
-//   writeString(view, 0, 'RIFF');
-//   /* file length */
-//   view.setUint32(4, 32 + samples.length, true);
-//   /* RIFF type */
-//   writeString(view, 8, 'WAVE');
-//   /* format chunk identifier */
-//   writeString(view, 12, 'fmt ');
-//   /* format chunk length */
-//   view.setUint32(16, 16, true);
-//   /* sample format (raw) */
-//   view.setUint16(20, 1, true);
-//   /* channel count */
-//   view.setUint16(22, 1, true);
-//   /* sample rate */
-//   view.setUint32(24, sampleRate, true);
-//   /* byte rate (sample rate * block align) */
-//   view.setUint32(28, sampleRate * 2, true);
-//   /* block align (channel count * bytes per sample) */
-//   view.setUint16(32, 2, true);
-//   /* bits per sample */
-//   view.setUint16(34, 8, true);
-//   /* data chunk identifier */
-//   writeString(view, 36, 'data');
-//   /* data chunk length */
-//   view.setUint32(40, samples.length, true);
-
-//   floatTo8BitPCM(view, 44, samples);
-
-//   return view;
-// }
